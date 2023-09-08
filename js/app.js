915f6cc80d9fc25f06646b55121022f5351b3716
Sherzod-Tohirov <sherzodtohirov862@gmail.com>
2023-09-08 23:47:18 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,63 @@
+if(!sessionStorage.getItem('token')) {
+    window.location.href = './login.html';
+}
+
+if(sessionStorage.getItem('token')) {
+    const elHeaderBtnWrapper = document.querySelector('.js-header-btn-wrapper');
+    const elHeaderBtnTemp = document.querySelector('.js-header-btn-temp').content.cloneNode(true);
+    elHeaderBtnWrapper.appendChild(elHeaderBtnTemp);
+
+}
+
+const elHeaderBtn = document.querySelector('.js-header-btn');
+const elHeaderList = document.querySelector('.js-header-list');
+const elLogoutBtn = document.querySelector('.js-logout-btn');
+const elUserTemp = document.querySelector('.js-user-item-temp');
+const elList = document.querySelector('.js-list');
+const fragment = document.createDocumentFragment();
+
+elHeaderBtn.addEventListener('click', (evt) => {
+    elHeaderList.classList.toggle('appear');
+});
+
+elLogoutBtn.addEventListener('click', (evt) => {
+    sessionStorage.removeItem('token');
+    window.location.href = './login.html';
+}); 
+
+
+
+
+getUsers('https://reqres.in/api/users');
+
+
+
+function renderUsers(data, list) {
+    list.innerHTML = '';
+
+    data.forEach(item => {
+        let temp = elUserTemp.content.cloneNode(true);
+        temp.querySelector('.users__img').src = item.avatar;
+        temp.querySelector('.users__img').alt = item.first_name + " image";
+        temp.querySelector('.user-name').textContent = item.first_name + ' ' + item.last_name;
+        temp.querySelector('.user-email').textContent = item.email;
+        temp.querySelector('.user-email').href = `mailto:${item.email}`;
+
+        fragment.appendChild(temp);
+        
+    });
+
+    list.appendChild(fragment);
+    
+}
+
+
+async function getUsers(url) {
+    try {
+       let res = await fetch(url);
+       let data = await res.json();
+       renderUsers(data.data, elList);
+    }catch(err) {
+        console.log(err);
+    }
+}
---
+++ js/login.js
@@ -0,0 +1,76 @@
+const elLoginForm = document.querySelector('.js-login-form');
+const elEmailInp = document.querySelector('.js-login-email');
+const elPasswordInp = document.querySelector('.js-login-password');
+const elErrorEmail = document.querySelector('.js-error-email');
+const elErrorPassword = document.querySelector('.js-error-password');
+
+
+
+
+
+elLoginForm.addEventListener('submit', (evt) => {
+
+  
+  evt.preventDefault();
+
+  let email = elEmailInp.value.trim();
+  let pass  = elPasswordInp.value.trim();
+
+  if(email.length == 0) {
+    elEmailInp.classList.add('is-invalid');
+    elErrorEmail.textContent = "Please enter your email !";
+    return;
+  }else if(!(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email))) {
+    elEmailInp.classList.add('is-invalid');
+    elErrorEmail.textContent = "Email must be valid !";
+    return;
+  }else {
+    elEmailInp.classList.remove('is-invalid');
+    elErrorEmail.textContent = '';
+  }
+
+  if(pass.length == 0) {
+    elPasswordInp.classList.add('is-invalid');
+    elErrorPassword.textContent = "Please enter your password !";
+    return;
+  }else {
+    elPasswordInp.classList.remove('is-invalid');
+    elErrorPassword.textContent = '';
+  }
+
+  postUserData('https://reqres.in/api/login', email, pass);
+
+
+
+
+});
+
+
+
+
+
+
+async function postUserData(url, email, password) {
+   try {
+    let res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email,
+          password
+        }),
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    })
+
+
+    let data = await res.json();
+    
+    if(data.token) {
+        sessionStorage.setItem('token', data.token);
+        window.location.href = './index.html';
+    }
+   }catch(err) {
+    console.log(err);
+   }
+} 
